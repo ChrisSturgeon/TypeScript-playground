082a3182418dd3e11c9e71b973a1896af6bd854e
csturgeon <sturgeon.chris@gmail.com>
2023-01-04 17:14:38 +0000

    Add basic files

---
+++ sandbox.js
@@ -0,0 +1,12 @@
+var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+//character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 50;
+isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,19 @@
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+//character = 20;
+
+character = 'luigi';
+
+// age = 'yoshi';
+
+age = 50;
+
+isBlackBelt = false;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+console.log(circ(7.5));
